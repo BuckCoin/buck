0b50ff27d57682f1de819d3221f4ff55aef83944
unknown <BuckCoinOrg@gmail.com>
2019-08-13 15:30:27 -0400

    Deprecation revision

--- src/deprecation.h
+++ src/deprecation.h
@@ -8,12 +8,12 @@
 // Deprecation policy:
 // * Shut down 16 weeks' worth of blocks after the estimated release block height.
 // * A warning is shown during the 2 weeks' worth of blocks prior to shut down.
-static const int APPROX_RELEASE_HEIGHT = 260000;
-static const int WEEKS_UNTIL_DEPRECATION = 16;
+static const int APPROX_RELEASE_HEIGHT = 323000;
+static const int WEEKS_UNTIL_DEPRECATION = 52;
 static const int DEPRECATION_HEIGHT = APPROX_RELEASE_HEIGHT + (WEEKS_UNTIL_DEPRECATION * 7 * 24 * 24);
 
 // Number of blocks before deprecation to warn users
-static const int DEPRECATION_WARN_LIMIT = 14 * 24 * 24; // 2 weeks
+static const int DEPRECATION_WARN_LIMIT = 24 * 24 * 24; // 4 weeks
 
 /**
  * Checks whether the node is deprecated based on the current block height, and
