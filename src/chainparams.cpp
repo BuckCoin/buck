ba74ab93ac75e302cf29b25494a1e202ad862a67
BuckCoin <35340131+BuckCoin@users.noreply.github.com>
2020-10-01 13:46:05 -0400

    Correct to witness data issue

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -273,7 +273,6 @@ public:
 			
             1556575752,       // * UNIX timestamp of last checkpoint block
             697145,           // * total number of transactions between genesis and last checkpoint
-                              //   (the tx=... number in the SetBestChain debug.log lines)
             1525   // * estimated number of transactions per day after checkpoint
                               //   total number of tx / (checkpoint block height / (24 * 24))
         };
