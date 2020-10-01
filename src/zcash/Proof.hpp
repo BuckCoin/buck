e976d41aa32e3972bfddd1916f9e0e7845c11738
BUCK <35340131+BuckCoin@users.noreply.github.com>
2020-09-30 22:02:08 -0400

    Update Proof.hpp

--- src/zcash/Proof.hpp
+++ src/zcash/Proof.hpp
@@ -201,9 +201,9 @@ public:
 };
 
 static constexpr size_t GROTH_PROOF_SIZE = (
-    48 + // p_A
-    96 + // p_B
-    48); // p_C
+    48 + // π_A
+    96 + // π_B
+    48); // π_C
 
 typedef std::array<unsigned char, GROTH_PROOF_SIZE> GrothProof;
 // TODO: Because PHGRProof is listed first, using the default
